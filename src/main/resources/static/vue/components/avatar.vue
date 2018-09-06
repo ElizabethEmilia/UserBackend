a28da40d98b1f25a910a3539d3ada9d1a8857551
Miyuki <listencpp@gmail.com>
2018-09-06 22:44:40 +0800

    头像组件

---
+++ src/main/resources/static/vue/components/avatar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div :style="avatar ? { backgroundImage: 'url(' + avatar + ')' }:{}"
+             class="avatar float-left avatarpos">
+        </div>
+        <h3 style="color: white">{{ username }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "avatar",
+        avatar: window.config.avatar,
+        username: window.config.username,
+    }
+</script>
+
+<style scoped>
+
+</style>
