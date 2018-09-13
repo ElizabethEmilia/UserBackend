cfc2919149f03b5d30363c81fcd557a17daad421
Jiabin Feng <18434391712@163.com>
2018-09-13 10:54:50 +0800

    组管理 [2.5]

--- src/main/java/org/ruoxue/backend/mapper/TGroupMapper.java
+++ src/main/java/org/ruoxue/backend/mapper/TGroupMapper.java
@@ -1,8 +1,11 @@
 package org.ruoxue.backend.mapper;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 import org.ruoxue.backend.bean.TGroup;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -14,5 +17,26 @@ import org.ruoxue.backend.bean.TGroup;
  */
 @Mapper
 public interface TGroupMapper extends BaseMapper<TGroup> {
+
+//    查询组的列表
+    @Select("select * from t_group order by id DESC limit #{page}, #{size}")
+    List<Map<String, Object>> listGroup(@Param("page") Integer page, @Param("size") Integer size);
+
+    @Select("select id, name from t_group order by id desc")
+    List<Map<String, Object>> listSimple();
+
+    @Delete("delete from t_group where id = #{id}")
+    Integer removeGroup(@Param("id") Integer id);
+
+    @Update("update t_group set name = #{name}, remark = #{remark} where id = #{id}")
+    Integer updateGroup(@Param("id") Integer id, @Param("name") String name, @Param("remark") String remark);
+
+    @Select("select * from t_admin where gid = #{gid} order by id desc limit #{page}, #{size}")
+    List<Map<String, Object>> listGroupAdmin(@Param("gid") Integer gid, @Param("page") Integer page, @Param("size") Integer size);
+
+    @Select("select * from t_customer where aid in (select * from (select id from t_admin where gid = #{gid}) c) order by uid desc limit #{page}, #{size}")
+    List<Map<String, Object>> listGroupCustomer(@Param("gid") Integer gid, @Param("page") Integer page, @Param("size") Integer size);
+
+
 
 }
--- src/main/java/org/ruoxue/backend/service/ITGroupService.java
+++ src/main/java/org/ruoxue/backend/service/ITGroupService.java
@@ -1,5 +1,6 @@
 package org.ruoxue.backend.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.service.IService;
 import org.ruoxue.backend.bean.TGroup;
 
@@ -12,5 +13,19 @@ import org.ruoxue.backend.bean.TGroup;
  * @since 2018-09-11
  */
 public interface ITGroupService extends IService<TGroup> {
-	
+
+    Object listGroup(Integer page, Integer size);
+
+    Object listSimple();
+
+    Object groupAdd(JSONObject jsonObject);
+
+    Object groupRemove(Integer gid);
+
+    Object groupUpdate(Integer gid, String name, String remark);
+
+    Object listGroupAdmin(Integer gid, Integer page, Integer size);
+
+    Object listGroupCustomer(Integer gid, Integer page, Integer size);
+
 }
