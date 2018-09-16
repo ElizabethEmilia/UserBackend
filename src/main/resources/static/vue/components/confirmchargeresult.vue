ca5bf2aa17ebf213fa8ee0d1d2f25f58872719b9
Miyuki <listencpp@gmail.com>
2018-09-16 18:55:50 +0800

    添加付款结果模板网页

---
+++ src/main/resources/static/vue/components/confirmchargeresult.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "confirmchargeresult"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main/resources/static/vue/routers/orders/confirmorders.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "confirmorders"
+    }
+</script>
+
+<style scoped>
+
+</style>
